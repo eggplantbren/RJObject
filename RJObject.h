681f5b4b8ea9e47f1376dbfb57b6ef066f19112f
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-04-14 05:34:22 +1200

    Comments, declare constructor

--- RJObject.h
+++ RJObject.h
@@ -5,7 +5,10 @@
 * A class that implements basic birth-death Metropolis-Hastings
 * proposals using an exponential prior on the masses and a
 * uniform prior on the positions. This is designed for 1, 2,
-* or 3-dimensional objects.
+* or 3-dimensional objects. This class stores masses for
+* each of the components it has, but it does not store
+* any other properties. If your components have more properties
+* you may derive from this class or store them externally.
 */
 
 #include <vector>
@@ -20,6 +23,7 @@ class RJObject
 		std::vector<double> masses;
 
 	public:
+		RJObject(int num_dimensions, int max_num_objects);
 
 };
 
