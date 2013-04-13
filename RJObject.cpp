9b3c36c2f0460f2c8dd96d059b7dfa298313a232
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-04-14 05:43:42 +1200

    Assign integers

--- RJObject.cpp
+++ RJObject.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 RJObject::RJObject(int num_dimensions, int max_num_objects)
-:positions(max_num_objects, vector<double>(num_dimensions))
+:num_dimensions(num_dimensions)
+,max_num_objects(max_num_objects)
+,positions(max_num_objects, vector<double>(num_dimensions))
 ,masses(max_num_objects)
 {
 
