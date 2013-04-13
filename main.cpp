283761efa4ef70425dbde29469eb847ee18d9efd
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-04-14 10:30:18 +1200

    Print correctly

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include <ctime>
 #include <iostream>
 #include "RJObject.h"
+#include "MassDistributions/Exponential.h"
 
+using namespace DNest3;
 using namespace std;
 
 // Demonstration of the class
 int main()
 {
-	/*
 	RandomNumberGenerator::initialise_instance();
 	RandomNumberGenerator::get_instance().set_seed(time(0));
 
 	RJObject<Exponential> r(2, 100);
 	r.fromPrior();
-	cout<<r<<endl;*/
-	cout<<"Hello world!"<<endl;
+	r.print(cout); cout<<endl;
 	return 0;
 }
 
