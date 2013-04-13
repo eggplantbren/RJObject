8dc6665b07cee3531d7cf49bfa67422082cf3d04
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-04-14 05:35:08 +1200

    Implement constructor

--- RJObject.cpp
+++ RJObject.cpp
@@ -1,2 +1,11 @@
 #include "RJObject.h"
 
+using namespace std;
+
+RJObject::RJObject(int num_dimensions, int max_num_objects)
+:positions(max_num_objects, vector<double>(num_dimensions))
+,masses(max_num_objects)
+{
+
+}
+
