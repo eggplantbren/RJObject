dc1fc6d5d9ad4e1b1b10ab6939569dfc3e1ccb21
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-04-28 09:59:30 +1200

    perturb_parameters()

--- SpatialDistributions/Elliptical2D.cpp
+++ SpatialDistributions/Elliptical2D.cpp
@@ -43,6 +43,44 @@ void Elliptical2D::print(ostream& out) const
 double Elliptical2D::perturb_parameters()
 {
 	double logH = 0.;
+
+	double scale = pow(10., 1.5 - 6.*randomU());
+	int which = randInt(5);
+	if(which == 0)
+	{
+		xc += length_scale*scale*randn();
+		yc += length_scale*scale*randn();
+		xc = mod(xc - x_min, x_max - x_min) + x_min;
+		yc = mod(yc - y_min, y_max - y_min) + y_min;
+	}
+	else if(which == 1)
+	{
+		q = log(q);
+		q += log(5./0.2)*scale*randn();
+		q = mod(q - log(0.2), log(5./0.2)) + log(0.2);
+		q = exp(q);
+	}
+	else if(which == 2)
+	{
+		theta += 2.*M_PI*scale*randn();
+		theta = mod(theta, 2.*M_PI);
+		cos_theta = cos(theta);
+		sin_theta = sin(theta);
+	}
+	else if(which == 3)
+	{
+		mu_r = log(mu_r);
+		mu_r += log(100.)*scale*randn();
+		mu_r = mod(mu_r - log(0.01*length_scale),
+				log(100.)) + log(0.01*length_scale);
+		mu_r = exp(mu_r);
+	}
+	else
+	{
+		beta_r += 2.*scale*randn();
+		beta_r = mod(beta_r, 2.);
+	}
+
 	return logH;
 }
 
