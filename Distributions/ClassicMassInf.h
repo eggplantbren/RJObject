92124beaad6f12e104bd03b5314237de5abd5dd2
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-07-25 14:44:42 +1200

    All interface defined

--- Distributions/ClassicMassInf.h
+++ Distributions/ClassicMassInf.h
@@ -5,7 +5,28 @@
 
 class ClassicMassInf:public Distribution
 {
+	private:
+		// Limits
+		double x_min, x_max, y_min, y_max;
+		double mu_min, mu_max;
 
+		// Mean of exponential distribution for masses
+		double mu;
+
+		double perturb_parameters();
+
+	public:
+		ClassicMassInf(double x_min, double x_max,
+					double y_min, double y_max,
+					double mu_min, double mu_max);
+
+		void fromPrior() = 0;
+
+		double log_pdf(const std::vector<double>& position) const;
+		void from_uniform(std::vector<double>& vec) const;
+		void to_uniform(std::vector<double>& vec) const;
+
+		void print(std::ostream& out) const;
 };
 
 #endif
