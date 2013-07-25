93b8fa92ba6301517b8f4d368c491255c62cf29e
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-07-25 14:29:22 +1200

    Gutted main, needs to be rewritten with new system

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CFLAGS = -O2 -Wall -Wextra -ansi -pedantic -DNDEBUG
 LIBS = -ldnest3 -lgsl -lgslcblas -lboost_thread -lboost_system
 
 default:
-	g++ $(CFLAGS) -c *.cpp MassDistributions/*.cpp SpatialDistributions/*.cpp
+	g++ $(CFLAGS) -c *.cpp Distributions/*.cpp
 	g++ -o main *.o $(LIBS)
 	rm -f *.o
 
--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include "RJObject.h"
-#include "SpatialDistributions/Elliptical2D.h"
-#include "MassDistributions/Exponential.h"
+#include "RandomNumberGenerator.h"
 
 using namespace DNest3;
 using namespace std;
@@ -15,39 +14,7 @@ int main()
 	RandomNumberGenerator::initialise_instance();
 	RandomNumberGenerator::get_instance().set_seed(time(0));
 
-	// Make an object with 2 spatial dimensions, maximum
-	// of 100 components, and exponential prior on the masses
-	// given a mean, which is log-uniform between 1E-3 and 1E3.
-	RJObject<Elliptical2D, Exponential> r
-		(2, 500, Elliptical2D(-1., 1., -1., 1.), Exponential(1E-3, 1E3));
 
-	// Generate the object from the prior
-	r.fromPrior();
-
-	// Open an output file stream to save the samples
-	fstream fout("output.txt", ios::out);
-
-	// How many MCMC steps to do
-	int steps = 1000;
-
-	for(int i=0; i<steps; i++)
-	{
-		// Make a proposal
-		RJObject<Elliptical2D, Exponential> r2 = r;
-		double logH = r2.perturb();
-
-		// Accept the proposal?
-		if(randomU() <= exp(logH))
-			r = r2;
-
-		if(i%1 == 0)
-		{
-			r.print(fout);
-			fout<<endl;
-		}
-		cout<<(i+1)<<'/'<<steps<<endl;
-	}
-	fout.close();
 	return 0;
 }
 
