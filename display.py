00ae6d546386730039f534ce9eb1df40c0529ae2
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-08-10 09:54:13 +1200

    Fixed display script

--- display.py
+++ display.py
@@ -2,15 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 output = np.atleast_2d(np.loadtxt('output.txt'))
-N = 100
+
+num_hyperparameters = 1
+num_dimensions = output[0, 0]
+max_num_components = output[0, 1]
 
 plt.ion()
 plt.figure()
 plt.hold(False)
 for i in xrange(0, output.shape[0]):
-	x = output[i, 4:(4+3*N):3]
-	y = output[i, 5:(5+3*N):3]
-	m = output[i, 6:(6+3*N):3]
+	x = output[i, (3 + num_hyperparameters):(3 + num_hyperparameters+max_num_components)]
+	y = output[i, (3 + num_hyperparameters + max_num_components):(3 + num_hyperparameters+2*max_num_components)]
+	m = output[i, (3 + num_hyperparameters + 2*max_num_components):(3 + num_hyperparameters + 3*max_num_components)]
 
 	x = x[m > 0.]
 	y = y[m > 0.]
