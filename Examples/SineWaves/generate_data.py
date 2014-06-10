bfb6abe5bb941f1c5162bacaba8a8c16a4b57881
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-06-10 14:29:22 +1200

    Generate sinewave data with phase transition

---
+++ Examples/SineWaves/generate_data.py
@@ -0,0 +1,41 @@
+from pylab import *
+
+"""
+Original example with two phase transations, but they do not interfere
+with the posterior.
+"""
+seed(123)
+
+t = linspace(0., 100., 1001)
+y = sin(2.*pi*t/30.) + sin(2.*pi*t/2. + 1.)
+sig = 1.
+Y = y + sig*randn(y.size)
+
+data = empty((t.size, 3))
+data[:,0], data[:,1], data[:,2] = t, Y, sig
+
+savetxt('fake_data.txt', data)
+
+plot(t, y, 'r')
+plot(t, Y, 'bo')
+show()
+
+
+"""
+This one has a phase transition at temperature = 1 :-D
+"""
+
+t = linspace(0., 100., 1001)
+y = sin(2.*pi*t/30.) + 0.3*sin(2.*pi*t/2. + 1.)
+sig = 1.
+Y = y + sig*randn(y.size)
+
+data = empty((t.size, 3))
+data[:,0], data[:,1], data[:,2] = t, Y, sig
+
+savetxt('fake_data.txt', data)
+
+plot(t, y, 'r')
+plot(t, Y, 'bo')
+show()
+
