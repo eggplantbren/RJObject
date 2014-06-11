263c88d0359892a444d695ba00b71807964cc540
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-06-12 11:06:21 +1200

    Load data

--- Examples/SineWaves/Data.cpp
+++ Examples/SineWaves/Data.cpp
@@ -1,4 +1,8 @@
 #include "Data.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
 
 Data::Data()
 {
@@ -7,6 +11,24 @@ Data::Data()
 
 void Data::load(const char* filename)
 {
+	fstream fin(filename, ios::in);
+	if(!fin)
+	{
+		cerr<<"# Error. Couldn't open file "<<filename<<"."<<endl;
+		return;
+	}
 
+	// Empty the vectors
+	t.clear();
+	y.clear();
+
+	double temp1, temp2;
+	while(fin>>temp1 && fin>>temp2)
+	{
+		t.push_back(temp1);
+		y.push_back(temp2);
+	}
+	cout<<"# Loaded "<<t.size()<<" data points from file "<<filename<<endl;
+	fin.close();
 }
 
--- Examples/SineWaves/Data.h
+++ Examples/SineWaves/Data.h
@@ -1,13 +1,20 @@
 #ifndef _Data_
 #define _Data_
 
+#include <vector>
+
 class Data
 {
 	private:
+		std::vector<double> t, y;
 
 	public:
 		Data();
 		void load(const char* filename);
+
+		// Getters
+		const std::vector<double>& get_t() const { return t; }
+		const std::vector<double>& get_y() const { return y; }
 
 	// Singleton
 	private:
