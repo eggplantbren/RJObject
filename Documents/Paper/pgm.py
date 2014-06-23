ee37ff0eb34e0ba5dc18487af5b5103e11ed3e41
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-06-24 10:26:26 +1200

    Bold alpha

--- Documents/Paper/pgm.py
+++ Documents/Paper/pgm.py
@@ -2,16 +2,18 @@
 PGM for the general problem using daft (http://daft-pgm.org/)
 """
 
+import matplotlib
 from matplotlib import rc
 rc("font", family="serif", size=12)
 rc("text", usetex=True)
+matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
 
 import daft
 
 pgm = daft.PGM([5., 4.], origin=[-2.5, -1.5])
 
 # Create the nodes
-pgm.add_node(daft.Node('alpha', r'$\alpha$', 0., 2.))
+pgm.add_node(daft.Node('alpha', r'$\boldsymbol{\alpha}$', 0., 2.))
 pgm.add_node(daft.Node('x', r'$\mathbf{x}_i$', 0., 0.))
 pgm.add_node(daft.Node('data', r'$\mathcal{D}$', 2., 0., observed=True))
 pgm.add_node(daft.Node('N', r'$N$', -2., 0.))
