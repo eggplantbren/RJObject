62e96a3a988e4d4ad0fff7e95e18b3d7187eacbb
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-06-23 16:54:23 +1200

    A pgm

---
+++ Documents/Paper/pgm.py
@@ -0,0 +1,29 @@
+"""
+PGM for the general problem using daft (http://daft-pgm.org/)
+"""
+
+from matplotlib import rc
+rc("font", family="serif", size=12)
+rc("text", usetex=True)
+
+import daft
+
+pgm = daft.PGM([4., 4.], origin=[-1.5, -1.5])
+
+# Create the nodes
+pgm.add_node(daft.Node('alpha', r'$\alpha$', 0., 2.))
+pgm.add_node(daft.Node('x', r'$x_i$', 0., 0.))
+pgm.add_node(daft.Node('data', r'$\mathcal{D}$', 2., 0., observed=True))
+
+# Add the edges
+pgm.add_edge('alpha', 'x')
+pgm.add_edge('x', 'data')
+
+# Add the plates
+pgm.add_plate(daft.Plate([-1., -1., 2., 2.], label=r'Objects $i=1, ..., N$'))
+
+pgm.render()
+pgm.figure.savefig("pgm.eps")
+pgm.figure.savefig("pgm.pdf")
+
+
