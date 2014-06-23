789fd1d24b256dbfb5710d6f89514a07b285e7f2
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-06-24 09:23:30 +1200

    Added N node

--- Documents/Paper/pgm.py
+++ Documents/Paper/pgm.py
@@ -8,16 +8,19 @@ rc("text", usetex=True)
 
 import daft
 
-pgm = daft.PGM([4., 4.], origin=[-1.5, -1.5])
+pgm = daft.PGM([5., 4.], origin=[-2.5, -1.5])
 
 # Create the nodes
 pgm.add_node(daft.Node('alpha', r'$\alpha$', 0., 2.))
 pgm.add_node(daft.Node('x', r'$\mathbf{x}_i$', 0., 0.))
 pgm.add_node(daft.Node('data', r'$\mathcal{D}$', 2., 0., observed=True))
+pgm.add_node(daft.Node('N', r'$N$', -2., 0.))
+pgm.add_node(daft.Node('hidden', r'', -1., 0., scale=0, plot_params={'alpha':0}))
 
 # Add the edges
 pgm.add_edge('alpha', 'x')
 pgm.add_edge('x', 'data')
+pgm.add_edge('N', 'hidden')
 
 # Add the plates
 pgm.add_plate(daft.Plate([-1., -1., 2., 2.], label=r'Objects $i=1, ..., N$'))
