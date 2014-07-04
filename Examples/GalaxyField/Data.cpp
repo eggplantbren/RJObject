5e26842c48bd6de5ac406c84e06133bc60c5204e
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-07-04 18:26:01 +1200

    Data class copied from Lensing2, but without PSF stuff

--- Examples/GalaxyField/Data.cpp
+++ Examples/GalaxyField/Data.cpp
@@ -1,6 +1,7 @@
 #include "Data.h"
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <cmath>
 
 using namespace std;
 
@@ -11,27 +12,75 @@ Data::Data()
 
 }
 
-void Data::load(const char* filename)
+void Data::load(const char* metadata_file, const char* image_file,
+			const char* sigma_file, const char* psf_file)
 {
-	fstream fin(filename, ios::in);
+	/*
+	* First, read in the metadata
+	*/
+	int psf_size;
+	fstream fin(metadata_file, ios::in);
 	if(!fin)
-	{
-		cerr<<"# Error. Couldn't open file "<<filename<<"."<<endl;
-		return;
-	}
-
-	// Empty the vectors
-	t.clear();
-	y.clear();
-
-	double temp1, temp2;
-	while(fin>>temp1 && fin>>temp2)
-	{
-		t.push_back(temp1);
-		y.push_back(temp2);
-	}
-	cout<<"# Loaded "<<t.size()<<" data points from file "
-			<<filename<<"."<<endl;
+		cerr<<"# ERROR: couldn't open file "<<metadata_file<<"."<<endl;
+	fin>>ni>>nj;
+	fin>>x_min>>x_max>>y_min>>y_max;
 	fin.close();
+
+	// Make sure maximum > minimum
+	if(x_max <= x_min || y_max <= y_min)
+		cerr<<"# ERROR: strange input in "<<metadata_file<<"."<<endl;
+
+	// Compute pixel widths
+	dx = (x_max - x_min)/nj;
+	dy = (y_max - y_min)/ni;
+
+	// Check that pixels are square
+	if(abs(log(dx/dy)) >= 1E-3)
+		cerr<<"# ERROR: pixels aren't square."<<endl;
+
+	compute_ray_grid();
+
+	/*
+	* Now, load the image
+	*/
+	fin.open(image_file, ios::in);
+	if(!fin)
+		cerr<<"# ERROR: couldn't open file "<<image_file<<"."<<endl;
+	image.assign(ni, vector<double>(nj));
+	for(size_t i=0; i<image.size(); i++)
+		for(size_t j=0; j<image[i].size(); j++)
+			fin>>image[i][j];
+	fin.close();
+
+	/*
+	* Load the sigma map
+	*/
+	fin.open(sigma_file, ios::in);
+	if(!fin)
+		cerr<<"# ERROR: couldn't open file "<<sigma_file<<"."<<endl;
+	sigma.assign(ni, vector<double>(nj));
+	for(size_t i=0; i<sigma.size(); i++)
+		for(size_t j=0; j<sigma[i].size(); j++)
+			fin>>sigma[i][j];
+	fin.close();
+}
+
+void Data::compute_ray_grid()
+{
+	// Make vectors of the correct size
+	x_rays.assign(ni, vector<double>(nj));
+	y_rays.assign(ni, vector<double>(nj));
+
+	// Distance between adjacent rays
+	double L = dx;
+
+	for(size_t i=0; i<x_rays.size(); i++)
+	{
+		for(size_t j=0; j<x_rays[i].size(); j++)
+		{
+			x_rays[i][j] = x_min + (j + 0.5)*L;
+			y_rays[i][j] = y_max - (i + 0.5)*L;
+		}
+	}
 }
 
