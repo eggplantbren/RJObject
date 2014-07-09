87c5836b46772cb7348eb8a4df58c99f44ac335f
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-07-09 15:22:33 +1200

    Plot GalaxyField data

---
+++ Examples/GalaxyField/make_plots.py
@@ -0,0 +1,27 @@
+from pylab import *
+
+rc("font", size=14, family="serif", serif="Computer Sans")
+rc("text", usetex=True)
+
+# Piecewise linear stretch
+def stretch(x):
+	y = x.copy()
+	y = (y - y.min())/(y.max() - y.min())
+	y[y > 0.1] = 0.1 + 0.05*(y[y > 0.1] - 0.1)
+	return y
+
+data = loadtxt('Data/test_image.txt')
+
+img = stretch(data)
+
+imshow(img, cmap='gray')
+xlabel('$x$')
+ylabel('$y$')
+gca().set_xticks([-0.5, 99.5, 199.5])
+gca().set_yticks([-0.5, 99.5, 199.5])
+gca().set_xticklabels(['-1', '0', '1'])
+gca().set_yticklabels(['1', '0', '-1'])
+title('GalaxyField Data')
+savefig('image.pdf', bbox_inches='tight')
+show()
+
