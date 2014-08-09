c807c7f68b08d5bd2f8e9a224be6693b7d08973e
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-08-09 19:20:51 +1200

    Bayes plots

---
+++ Documents/Talk/plots.py
@@ -0,0 +1,23 @@
+from pylab import *
+
+rc("font", size=16, family="serif", serif="Computer Sans")
+rc("text", usetex=True)
+
+x = linspace(0., 100., 1001)
+
+prior = 1./(x + 10.)**2
+prior = prior/trapz(prior, x=x)
+lik = 0.08*exp(-0.5*(x - 60)**2/10**2)
+post = prior*lik
+post = post/trapz(post, x=x)
+
+plot(x, prior, 'b', linewidth=3, label='Prior')
+plot(x, lik, 'c--', linewidth=2, label='Likelihood')
+plot(x, post, 'r', linewidth=3, label='Posterior')
+xlabel(r'$\theta$')
+ylabel('Probability Density')
+legend(loc='upper right')
+savefig('bayes.pdf', bbox_inches='tight')
+show()
+
+
