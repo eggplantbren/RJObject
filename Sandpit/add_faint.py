73bdb1c09e10511b54ae627bac7c99b3a4449258
Brendon J. Brewer <brendon.brewer@gmail.com>
2015-06-30 13:21:04 +1200

    Sandpit directory

---
+++ Sandpit/add_faint.py
@@ -0,0 +1,10 @@
+"""
+Looking for a method to enhance the probability that 'birth'
+proposals have lower amplitude than the prior would suggest.
+"""
+
+import numpy as np
+import numpy.random as rng
+import matplotlib.pyplot as plt
+
+
