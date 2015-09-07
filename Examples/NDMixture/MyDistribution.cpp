83e9a1c69897dbda7fa7797d0271ddff5b019a21
Brendon Brewer <brendon.brewer@gmail.com>
2015-09-07 19:35:21 +0100

    perturb method

--- Examples/NDMixture/MyDistribution.cpp
+++ Examples/NDMixture/MyDistribution.cpp
@@ -39,42 +39,43 @@ void MyDistribution::fromPrior()
 	diversity_logweights = 2.*randomU();
 }
 
-//double MyDistribution::perturb_parameters()
-//{
-//	double logH = 0.;
+double MyDistribution::perturb_parameters()
+{
+	double logH = 0.;
 
-//	int which = randInt(5);
+	int which = randInt(5);
+	int i = randInt(min.size());
 
-//	if(which == 0)
-//	{
-//		center_locations += x_range*randh();
-//		wrap(center_locations, x_min, x_max);
-//	}
-//	if(which == 1)
-//	{
-//		diversity_locations = log(diversity_locations/x_range);
-//		diversity_locations += log(1E3)*randh();
-//		wrap(diversity_locations, log(1E-3), log(1.));
-//		diversity_locations = exp(diversity_locations)*x_range;
-//	}
-//	if(which == 2)
-//	{
-//		center_logwidths += log(1E6)*randh();
-//		wrap(center_logwidths, log(1E-3*x_range), log(1E3*x_range));
-//	}
-//	if(which == 3)
-//	{
-//		diversity_logwidths += 2.*randh();
-//		wrap(diversity_logwidths, 0., 2.);
-//	}
-//	if(which == 4)
-//	{
-//		diversity_logweights += 2.*randh();
-//		wrap(diversity_logweights, 0., 2.);
-//	}
+	if(which == 0)
+	{
+		center_locations[i] += range[i]*randh();
+		wrap(center_locations[i], min[i], max[i]);
+	}
+	if(which == 1)
+	{
+		diversity_locations[i] = log(diversity_locations[i]/range[i]);
+		diversity_locations[i] += log(1E3)*randh();
+		wrap(diversity_locations[i], log(1E-3), log(1.));
+		diversity_locations[i] = exp(diversity_locations[i])*range[i];
+	}
+	if(which == 2)
+	{
+		center_logwidths[i] += log(1E6)*randh();
+		wrap(center_logwidths[i], log(1E-3*range[i]), log(1E3*range[i]));
+	}
+	if(which == 3)
+	{
+		diversity_logwidths[i] += 2.*randh();
+		wrap(diversity_logwidths[i], 0., 2.);
+	}
+	if(which == 4)
+	{
+		diversity_logweights += 2.*randh();
+		wrap(diversity_logweights, 0., 2.);
+	}
 
-//	return logH;
-//}
+	return logH;
+}
 
 //// vec[0] = central position
 //// vec[1] = log_width
