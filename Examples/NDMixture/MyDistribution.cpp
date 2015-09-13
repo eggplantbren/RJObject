73548ee60b4d465f5ca7698b16b7a522ac752ba0
Brendon Brewer <brendon.brewer@gmail.com>
2015-09-13 13:06:10 +0100

    Use Data::num_dimensions instead of vector sizes

--- Examples/NDMixture/MyDistribution.cpp
+++ Examples/NDMixture/MyDistribution.cpp
@@ -12,22 +12,22 @@ using namespace DNest3;
 MyDistribution::MyDistribution(const vector<double>& min, const vector<double>& max)
 :min(min)
 ,max(max)
-,range(max.size())
-,center_locations(max.size())
-,diversity_locations(max.size())
-,center_logwidths(max.size())
-,diversity_logwidths(max.size())
+,range(Data::num_dimensions)
+,center_locations(Data::num_dimensions)
+,diversity_locations(Data::num_dimensions)
+,center_logwidths(Data::num_dimensions)
+,diversity_logwidths(Data::num_dimensions)
 {
 	assert(min.size() == Data::num_dimensions);
 	assert(max.size() == Data::num_dimensions);
 
-	for(size_t i=0; i<range.size(); i++)
+	for(int i=0; i<Data::num_dimensions; i++)
 		range[i] = max[i] - min[i];
 }
 
 void MyDistribution::fromPrior()
 {
-	for(size_t i=0; i<min.size(); i++)
+	for(int i=0; i<Data::num_dimensions; i++)
 	{
 		center_locations[i] = min[i] + range[i]*randomU();
 		diversity_locations[i] = exp(log(1E-3) + log(1E3)*randomU())*range[i];
@@ -44,7 +44,7 @@ double MyDistribution::perturb_parameters()
 	double logH = 0.;
 
 	int which = randInt(5);
-	int i = randInt(min.size());
+	int i = randInt(Data::num_dimensions);
 
 	if(which == 0)
 	{
@@ -85,12 +85,12 @@ double MyDistribution::log_pdf(const std::vector<double>& vec) const
 {
 	double logp = 0.;
 
-	for(size_t i=0; i<min.size(); i++)
+	for(int i=0; i<Data::num_dimensions; i++)
 	{
 		logp += -log(diversity_locations[i])
 			-0.5*pow((vec[i] - center_locations[i])/diversity_locations[i], 2);
 		logp += -log(diversity_logwidths[i])
-			-0.5*pow((vec[min.size() + i] - center_logwidths[i])/diversity_logwidths[i], 2);
+			-0.5*pow((vec[Data::num_dimensions + i] - center_logwidths[i])/diversity_logwidths[i], 2);
 	}
 	logp += -log(diversity_logweights)
 		-0.5*pow(vec[2]/diversity_logweights, 2);
@@ -100,34 +100,34 @@ double MyDistribution::log_pdf(const std::vector<double>& vec) const
 
 void MyDistribution::from_uniform(std::vector<double>& vec) const
 {
-	for(size_t i=0; i<min.size(); i++)
+	for(int i=0; i<Data::num_dimensions; i++)
 	{
 		vec[i] = center_locations[i]
 				+ diversity_locations[i]*gsl_cdf_ugaussian_Pinv(vec[i]);
 
-		vec[min.size() + i] = center_logwidths[i]
-				+ diversity_logwidths[i]*gsl_cdf_ugaussian_Pinv(vec[min.size() + i]);
+		vec[Data::num_dimensions + i] = center_logwidths[i]
+				+ diversity_logwidths[i]*gsl_cdf_ugaussian_Pinv(vec[Data::num_dimensions + i]);
 	}
-	vec[min.size()] = diversity_logweights*gsl_cdf_ugaussian_Pinv(vec[min.size()]);
+	vec[Data::num_dimensions] = diversity_logweights*gsl_cdf_ugaussian_Pinv(vec[Data::num_dimensions]);
 }
 
 void MyDistribution::to_uniform(std::vector<double>& vec) const
 {
-	for(size_t i=0; i<min.size(); i++)
+	for(int i=0; i<Data::num_dimensions; i++)
 	{
 		vec[i] = gsl_cdf_ugaussian_P((vec[i] - center_locations[i])/diversity_locations[i]);
-		vec[min.size() + i] = gsl_cdf_ugaussian_P((vec[min.size() + i] - center_logwidths[i])/diversity_logwidths[i]);
+		vec[Data::num_dimensions + i] = gsl_cdf_ugaussian_P((vec[Data::num_dimensions + i] - center_logwidths[i])/diversity_logwidths[i]);
 	}
-	vec[min.size()] = gsl_cdf_ugaussian_P(vec[min.size()]/diversity_logweights);
+	vec[Data::num_dimensions] = gsl_cdf_ugaussian_P(vec[Data::num_dimensions]/diversity_logweights);
 }
 
 void MyDistribution::print(std::ostream& out) const
 {
-	for(size_t i=0; i<min.size(); i++)
+	for(int i=0; i<Data::num_dimensions; i++)
 		out<<center_locations[i]<<' ';
-	for(size_t i=0; i<min.size(); i++)
+	for(int i=0; i<Data::num_dimensions; i++)
 		out<<diversity_locations[i]<<' ';
-	for(size_t i=0; i<min.size(); i++)
+	for(int i=0; i<Data::num_dimensions; i++)
 		out<<center_logwidths[i]<<' ';
 	out<<diversity_logweights<<' ';
 }
