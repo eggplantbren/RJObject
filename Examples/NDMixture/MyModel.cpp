c88331d4bb7c492294c3fa2418cfdcca846e3e6f
Brendon Brewer <brendon.brewer@gmail.com>
2015-09-13 13:44:52 +0100

    log likelihood etc

--- Examples/NDMixture/MyModel.cpp
+++ Examples/NDMixture/MyModel.cpp
@@ -29,57 +29,61 @@ double MyModel::perturb()
 	return logH;
 }
 
-//double MyModel::logLikelihood() const
-//{
-//	// Get the data
-//	const vector<double>& x = Data::get_instance().get_x();
+double MyModel::logLikelihood() const
+{
+	// Get the data
+	const vector< vector<double> >& x = Data::get_instance().get_x();
 
-//	// Get mixture components
-//	const vector< vector<double> >& components = objects.get_components();
+	// Get mixture components
+	const vector< vector<double> >& components = objects.get_components();
 
-//	// Can't have zero mixture components
-//	if(components.size() == 0)
-//		return -1E200;
+	// Can't have zero mixture components
+	if(components.size() == 0)
+		return -1E200;
 
-//	// Calculate normalised log weights of mixture components
-//	vector<double> logw(components.size());
-//	double tot = -1E300;
-//	for(size_t i=0; i<components.size(); i++)
-//	{
-//		logw[i] = components[i][2];
-//		tot = logsumexp(tot, logw[i]);
-//	}
-//	for(size_t i=0; i<components.size(); i++)
-//		logw[i] -= tot;
+	// Calculate normalised log weights of mixture components
+	vector<double> logw(components.size());
+	double tot = -1E300;
+	for(size_t i=0; i<components.size(); i++)
+	{
+		logw[i] = components[i].back();
+		tot = logsumexp(tot, logw[i]);
+	}
+	for(size_t i=0; i<components.size(); i++)
+		logw[i] -= tot;
 
-//	double logL = 0.;
+	double logL = 0.;
 
-//	// Loop over datapoints
-//	for(size_t i=0; i<x.size(); i++)
-//	{
-//		// Loop over mixture components
-//		double temp = -1E300;
-//		double gaussian;
-//		for(size_t j=0; j<components.size(); j++)
-//		{
-//			gaussian = -0.5*log(2.*M_PI) - components[j][1]
-//				-0.5*pow((x[i] - components[j][0])/exp(components[j][1]), 2);
-//			temp = logsumexp(temp, logw[j] + gaussian);
-//		}
+	double temp, gaussian;
+	// Loop over datapoints
+	for(size_t i=0; i<x.size(); i++)
+	{
+		// Loop over dimensions
+		for(size_t j=0; j<x[i].size(); j++)
+		{
+			// Loop over mixture components
+			temp = -1E300;
+			for(size_t k=0; k<components.size(); k++)
+			{
+				gaussian = -0.5*log(2.*M_PI) - components[k][j + Data::num_dimensions]
+					-0.5*pow((x[i][j] - components[k][j])/exp(components[k][j + Data::num_dimensions]), 2);
+				temp = logsumexp(temp, logw[k] + gaussian);
+			}
 
-//		logL += temp;
-//	}
+			logL += temp;
+		}
+	}
 
-//	return logL;
-//}
+	return logL;
+}
 
-//void MyModel::print(std::ostream& out) const
-//{
-//	objects.print(out); out<<' ';
-//}
+void MyModel::print(std::ostream& out) const
+{
+	objects.print(out); out<<' ';
+}
 
-//string MyModel::description() const
-//{
-//	return string("objects");
-//}
+string MyModel::description() const
+{
+	return string("objects");
+}
 
