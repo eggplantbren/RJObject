342d5f9fd45bcc795b8dcf3ac0d1f7951c41477a
Brendon J. Brewer <brendon.brewer@gmail.com>
2015-10-12 03:17:19 +1300

    grey colormap

--- Examples/GalaxyField/display.py
+++ Examples/GalaxyField/display.py
@@ -22,7 +22,7 @@ hold(False)
 for i in xrange(0, posterior_sample.shape[0]):
 	img = posterior_sample[i, 0:200**2].reshape((200, 200))
 	subplot(1, 2, 1)
-	imshow(stretch(img))
+	imshow(-stretch(img), cmap='gray')
 	title('Model {i}'.format(i=i))
 	gca().set_xticks([-0.5, 99.5, 199.5])
 	gca().set_yticks([-0.5, 99.5, 199.5])
@@ -31,7 +31,7 @@ for i in xrange(0, posterior_sample.shape[0]):
 
 	subplot(1, 2, 2)
 	sigma = sqrt(sig**2 + posterior_sample[i,-2]**2)
-	imshow((img - data)/sigma)
+	imshow(-(img - data)/sigma, cmap='gray')
 	title('Standardised Residuals')
 	gca().set_xticks([-0.5, 99.5, 199.5])
 	gca().set_yticks([-0.5, 99.5, 199.5])
